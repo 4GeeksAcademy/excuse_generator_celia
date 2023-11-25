83d470754d2ddbd20daf823e79ffe64b426cebcb
Celia Elias <148253357+celiaelias@users.noreply.github.com>
2023-11-25 01:19:25 +0000

    feat: excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
+  //excuseGenerator();
 };
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = numberGenerator(who.length);
+  let actionIndex = numberGenerator(action.length);
+  let whatIndex = numberGenerator(what.length);
+  let whenIndex = numberGenerator(when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
+
+function numberGenerator(numberLength) {
+  return Math.floor(Math.random() * numberLength);
+}
